17c1313670fe931c5af295cf47d41a366d9b8aa4
Bart Vrancken <bart@abuse.io>
2015-12-22 22:45:53 +0100

    add method to return collector object

--- src/Factory.php
+++ src/Factory.php
@@ -3,6 +3,7 @@
 namespace AbuseIO\Collectors;
 
 use Symfony\Component\ClassLoader\ClassMapGenerator;
+use Log;
 
 class Factory
 {
@@ -39,5 +40,42 @@ class Factory
             }
         }
         return $collectors;
+    }
+
+    /**
+     * Create and return a Collector class and it's configuration
+     * @return class
+     */
+    public static function create($requiredName)
+    {
+        /**
+         * Loop through the collector list and try to find a match by name
+         */
+        $collectors = Factory::getCollectors();
+        foreach ($collectors as $collectorName) {
+
+            if ($collectorName === ucfirst($requiredName)) {
+                $collectorClass = 'AbuseIO\\Collectors\\' . $collectorName;
+
+                // Collector is enabled, then return its object
+                if (config("collectors.{$collectorName}.collector.enabled") === true) {
+
+                    return new $collectorClass();
+
+                } else {
+                    Log::info(
+                        '(JOB ' . getmypid() . ') \AbuseIO\Collectors\Factory: ' .
+                        "The collector {$collectorName} has been disabled and will not be used."
+                    );
+                }
+            }
+        }
+
+        // No valid collectors found
+        Log::info(
+            '(JOB ' . getmypid() . ') \AbuseIO\Collectors\Factory: ' .
+            "The collector {$collectorName} is not present on this system"
+        );
+        return false;
     }
 }
